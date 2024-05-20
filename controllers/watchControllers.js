3d8c27b802e7db70f265b0e1d33a32d118828578
MeoKool <truongsyquang1202@gmail.com>
2024-05-20 21:44:51 +0700

    getWatchesByName

--- controllers/watchControllers.js
+++ controllers/watchControllers.js
@@ -10,6 +10,18 @@ const watchesControllers = {
       res.status(500).json({ message: error.message });
     }
   },
+  // getWatchByName
+  getWatchByName: async (req, res) => {
+    try {
+      const watch = await Watches.findOne({ watchName: req.params.name });
+      if (!watch) {
+        return res.status(404).json({ message: "Watch not found" });
+      }
+      res.status(200).json(watch);
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  },
 };
 
 module.exports = watchesControllers;
--- routes/watch.js
+++ routes/watch.js
@@ -3,5 +3,6 @@ const watchesControllers = require("../controllers/watchControllers");
 const router = require("express").Router();
 
 router.get("/getAllWatches", watchesControllers.getAllWatches);
+router.get("/getWatchByName/:name", watchesControllers.getWatchByName);
 
 module.exports = router;
