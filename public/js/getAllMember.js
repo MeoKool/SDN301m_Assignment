6295f8897d246761c71085fbd2a6c1b622a185ee
MeoKool <truongsyquang1202@gmail.com>
2024-05-30 23:51:48 +0700

    getAllMember

--- public/js/getAllMember.js
+++ public/js/getAllMember.js
@@ -14,14 +14,24 @@ window.onload = function () {
 
 function displayMembers(members) {
   const tableBody = document.querySelector("tbody");
+  let count = 1;
   members.forEach((member) => {
     const row = document.createElement("tr");
     const memberNameCell = document.createElement("td");
+    const countCell = document.createElement("td");
+    countCell.textContent = count++;
     memberNameCell.textContent = member.memberName;
     const nameCell = document.createElement("td");
     nameCell.textContent = member.name;
     const yobCell = document.createElement("td");
-    yobCell.textContent = member.yob;
+    const date = new Date(member.yob);
+    const formattedDate = `${date.getDate().toString().padStart(2, "0")}/${(
+      date.getMonth() + 1
+    )
+      .toString()
+      .padStart(2, "0")}/${date.getFullYear()}`;
+    yobCell.textContent = formattedDate;
+    row.appendChild(countCell);
     row.appendChild(memberNameCell);
     row.appendChild(nameCell);
     row.appendChild(yobCell);
